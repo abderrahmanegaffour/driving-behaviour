b7cd7351229f7f4769fea44745951e69f57b2ab0
Gaffour abderrahmane <62155626+abderrahmanegaffour@users.noreply.github.com>
2022-06-07 11:48:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
 echo "\
+[theme]\n\
+base = ’light’\n\
+primaryColor = ‘#2696bd’\n\
+secondaryBackgroundColor = ‘#ecebeb’\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
